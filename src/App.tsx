f0c8195d416c5bd12279fdac2ff9b031e539b9c9
adjong87 <adjong87@gmail.com>
2023-01-28 21:18:37 +0100

    Added graphql and storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
+import { API, Storage } from 'aws-amplify';
 import {
     Button,
     Flex,
     Heading,
+    Image,
     Text,
     TextField,
     View,
     withAuthenticator,
-} from "@aws-amplify/ui-react";
+} from '@aws-amplify/ui-react';
 import { listNotes } from "./graphql/queries";
 import {
     createNote as createNoteMutation,
@@ -27,16 +28,28 @@ const App = ({ signOut }) => {
     async function fetchNotes() {
         const apiData = await API.graphql({ query: listNotes });
         const notesFromAPI = apiData.data.listNotes.items;
+        await Promise.all(
+            notesFromAPI.map(async (note) => {
+                if (note.image) {
+                    const url = await Storage.get(note.name);
+                    note.image = url;
+                }
+                return note;
+            })
+        );
         setNotes(notesFromAPI);
     }
 
     async function createNote(event) {
         event.preventDefault();
         const form = new FormData(event.target);
+        const image = form.get("image");
         const data = {
             name: form.get("name"),
             description: form.get("description"),
+            image: image.name,
         };
+        if (!!data.image) await Storage.put(data.name, image);
         await API.graphql({
             query: createNoteMutation,
             variables: { input: data },
@@ -45,9 +58,10 @@ const App = ({ signOut }) => {
         event.target.reset();
     }
 
-    async function deleteNote({ id }) {
+    async function deleteNote({ id, name }) {
         const newNotes = notes.filter((note) => note.id !== id);
         setNotes(newNotes);
+        await Storage.remove(name);
         await API.graphql({
             query: deleteNoteMutation,
             variables: { input: { id } },
@@ -78,8 +92,15 @@ const App = ({ signOut }) => {
                     <Button type="submit" variation="primary">
                         Create Note
                     </Button>
+
                 </Flex>
             </View>
+            <View
+                name="image"
+                as="input"
+                type="file"
+                style={{ alignSelf: "end" }}
+            />
             <Heading level={2}>Current Notes</Heading>
             <View margin="3rem 0">
                 {notes.map((note) => (
@@ -93,10 +114,18 @@ const App = ({ signOut }) => {
                             {note.name}
                         </Text>
                         <Text as="span">{note.description}</Text>
+                        {note.image && (
+                            <Image
+                                src={note.image}
+                                alt={`visual aid for ${notes.name}`}
+                                style={{ width: 400 }}
+                            />
+                        )}
                         <Button variation="link" onClick={() => deleteNote(note)}>
                             Delete note
                         </Button>
                     </Flex>
+                ))}
                 ))}
             </View>
             <Button onClick={signOut}>Sign Out</Button>
